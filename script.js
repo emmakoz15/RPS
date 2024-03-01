c2a65ff75d26e844fd4f5b0ba405f99fb3c10bf7
emmakoz15 <emmakozlovsky@gmail.com>
2024-03-01 09:32:01 -0500

    added more js not done yet

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ window.onload = (event) =>{
     document.getElementById('yname').textContent = `${userName}`
 }
 
+
+
+//gm=ame score
+let playerScore = 0
+let computerScore = 0
+
 //play game
 
 const choices = ["rock", "paper", "scissors"]
@@ -26,8 +32,7 @@ function playGame(playerChoice){
 
   if (playerChoice === computerChoice){
     result = "IT'S A TIE"
-  }
-  else{
+  }  else{
     switch(playerChoice){
       case 'rock' :
       result = (computerChoice === 'scissors') ? "you win": "you lose"
@@ -41,14 +46,39 @@ function playGame(playerChoice){
     }
   }
 
+  if (result === "you win"){
+    playerScore++ //you win your score goes up 1
+  } else if (result === "you lose") {
+    computerScore++ //comp wins it goes up 1
+  } else if( playerScore + computerScore === 5){
+    document.getElementById("rock").classList.add("display")
+    document.getElementById("paper").classList.add("display")
+    document.getElementById("Scissor").classList.add("display")
+} else {
+  
+}
+   // nothing gets added
+  
+  
+ if(playerScore > 3){
+  document.getElementById("winorlose").textContent = "You WIN"
+} else if(computerScore > 3){
+document.getElementById("winorlose").textContent = "You LOSE"
+} else{
+  
+}
+
+ 
+
   playerDisplay.textContent = `Player ${playerChoice}`
   computerDisplay.textContent = `Computer ${computerChoice}`
   resultDisplay.textContent = result
 
-  
+  document.getElementById('change').textContent = `${playerScore} vs. ${computerScore}`
+
+ 
 }
 // https://www.youtube.com/watch?v=3uKdQx-SZ5A&t=967s link for tutorial
 
 //collect score
 
-let calcScore = 
